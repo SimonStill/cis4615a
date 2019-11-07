80859540750134593b730974b7cda7f4aa47df76
SimonStill <57300255+SimonStill@users.noreply.github.com>
2019-11-07 18:17:51 -0500

    Add files via upload

---
+++ R00_IDS03.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Rule00 - IDS03
+ *
+ ******************************************************************************/
+
+
+if (loginSuccessful) {
+   logger.severe("User login succeeded for: " + username);
+}else{
+   logger.severe("User login failed for: " + username);
+}
+
+
+
+
